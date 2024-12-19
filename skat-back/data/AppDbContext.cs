a82c4b401bb382213be37b337a9a5fb810f45efa
Sean Leon Maier <sean.maier@outlook.de>
2024-12-20 00:25:29 +0100

    Adjusted Context to new Tables

--- skat-back/data/AppDbContext.cs
+++ skat-back/data/AppDbContext.cs
@@ -3,8 +3,26 @@ using skat_back.data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Match> Games { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Match> Matches { get; set; }
     public DbSet<Matchday> Matchdays { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Matchday)
+            .WithMany(m => m.Matches)
+            .HasForeignKey(m => m.MatchdayId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Player)
+            .WithMany(p => p.Matches)
+            .HasForeignKey(m => m.PlayerId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        base.OnModelCreating(modelBuilder);
+    }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
