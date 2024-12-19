5689bf69dc3a43bb4f178e0b7de7558fe5b63083
Sean Leon Maier <sean.maier@outlook.de>
2024-12-20 00:24:59 +0100

    Added DateTime for Player Table

--- skat-back/models/Player.cs
+++ skat-back/models/Player.cs
@@ -1,7 +1,13 @@
-﻿namespace skat_back.data;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace skat_back.data;
 
 public class Player
 {
-    public int Pid { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public List<Match> Matches { get; set; } = new List<Match>();
 }
