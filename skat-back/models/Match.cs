d1abf9fcfe9e059fda142137dd949a760ac13889
Sean Leon Maier <sean.maier@outlook.de>
2024-12-19 17:05:08 +0100

    Added Class Match

---
+++ skat-back/models/Match.cs
@@ -0,0 +1,9 @@
+﻿namespace skat_back.data;
+
+public class Match
+{
+    public int Id { get; }
+    public string Name { get; set; }
+    public int Won { get; set; }
+    public int Lost { get; set; }
+}
