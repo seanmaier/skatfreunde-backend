c8f811f54d04f7583d91a08470c6256a7db11067
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:27:55 +0100

    Added conditions for MatchDay class

--- skat-back/models/Matchday.cs
+++ skat-back/models/Matchday.cs
@@ -1,8 +1,12 @@
-﻿namespace skat_back.data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace skat_back.data;
 
 public class Matchday
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public DateTime Date { get; set; }
 
     public List<Match> Matches { get; set; } = new List<Match>();
