ec45573a383dde5b7c10b2d1f6191640502b2242
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:09:03 +0100

    Added conditions to Player table

--- skat-back/models/Player.cs
+++ skat-back/models/Player.cs
@@ -1,13 +1,19 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace skat_back.data;
 
 public class Player
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; }
+    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
 
     public List<Match> Matches { get; set; } = new List<Match>();
 }
