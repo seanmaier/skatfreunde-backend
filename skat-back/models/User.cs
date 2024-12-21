51426a7f61b393b052967c24f76dba01b0b3e078
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:04:05 +0100

    Added User Class

---
+++ skat-back/models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace skat_back.data;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string PasswordHash { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Email { get; set; }
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+}
