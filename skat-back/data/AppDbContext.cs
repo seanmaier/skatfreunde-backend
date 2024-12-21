7c40681681a6eff784d987c460f00aafeeb5f670
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:44:09 +0100

    Added BlogPosts to DbContext

--- skat-back/data/AppDbContext.cs
+++ skat-back/data/AppDbContext.cs
@@ -7,9 +7,12 @@ public class AppDbContext : DbContext
     public DbSet<Player> Players { get; set; }
     public DbSet<Match> Matches { get; set; }
     public DbSet<MatchDay> MatchDays { get; set; }
+    public DbSet<BlogPost> BlogPosts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        
+        // ===========Matches===========
         modelBuilder.Entity<Match>()
             .HasOne(m => m.MatchDay)
             .WithMany(m => m.Matches)
@@ -27,7 +30,21 @@ public class AppDbContext : DbContext
 
         modelBuilder.Entity<Match>()
             .HasIndex(m => m.PlayerId);
+
         
+        // ===========BlogPosts===========
+        modelBuilder.Entity<BlogPost>()
+            .HasIndex(b => b.Slug)
+            .IsUnique();
+
+        modelBuilder.Entity<BlogPost>()
+            .HasIndex(b => b.Status);
+
+        modelBuilder.Entity<BlogPost>()
+            .HasIndex(b => b.CreatedAt);
+        
+        
+        // ===========Users===========
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasIndex(u => u.Email).IsUnique();
