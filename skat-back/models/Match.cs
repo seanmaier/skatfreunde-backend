df273d1f2a0564d7b1305cd0ec098f550665a198
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:24:37 +0100

    Added conditions to Match class

--- skat-back/models/Match.cs
+++ skat-back/models/Match.cs
@@ -4,18 +4,24 @@ namespace skat_back.data;
 
 public class Match
 {
-    
+    [Key]
     public int Id { get; set; } 
-    public int MatchDayId { get; set; } 
+    
+    public int MatchDayId { get; set; }
     public Matchday MatchDay { get; set; }
     
     public int PlayerId { get; set; }
     public Player Player { get; set; }
     
+    [Required]
     public int Points { get; set; }
+    [Required]
     public int Won { get; set; }
+    [Required]
     public int Lost { get; set; }
+    [Required]
     public int Table { get; set; }
+    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
 }
