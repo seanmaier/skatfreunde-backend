5b749c55d6441397aa11405f8a1f8d0494551480
Sean Leon Maier <sean.maier@outlook.de>
2024-12-21 11:40:10 +0100

    Created BlogPost class

---
+++ skat-back/models/BlogPost.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace skat_back.data;
+
+public class BlogPost
+{
+    [Key] 
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Title { get; set; }
+    
+    [Required]
+    public string Text { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Slug { get; set; }
+    
+    [MaxLength(500)]
+    public string Summary { get; set; }
+
+    [Required] public string Status { get; set; } = "Draft"; 
+    
+    [MaxLength(255)]
+    public string MetaTitle { get; set; }
+    
+    [MaxLength(500)]
+    public string MetaDescription { get; set; }
+
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+}
