9b4d38fdabc8fbdc9b01140da00872b994fe53d0
Sean Leon Maier <sean.maier@outlook.de>
2024-12-22 21:40:42 +0100

    Added generalized CRUD operations

---
+++ skat-back/services/Repository.cs
@@ -0,0 +1,47 @@
+﻿namespace skat_back.services;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public T? GetById(int id)
+    {
+        return _context.Set<T>().Find(id);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+    public void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Update(int id, T updatedEntity, Action<T, T> updateAction)
+    {
+        var entity = _context.Set<T>().Find(id);
+
+        if (entity == null) return;
+
+        updateAction(entity, updatedEntity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var entity = _context.Set<T>().Find(id);
+        
+        if (entity == null) return;
+
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+    }
+}
