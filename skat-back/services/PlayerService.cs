440b1b3aeeb58818d4583942be020d9bf762dd63
Sean Leon Maier <sean.maier@outlook.de>
2024-12-22 22:03:01 +0100

    Added Generalized PlayerService

---
+++ skat-back/services/PlayerService.cs
@@ -0,0 +1,43 @@
+﻿using skat_back.data;
+using SQLitePCL;
+
+namespace skat_back.services;
+
+public class PlayerService
+{
+    private readonly Repository<Player> _repository;
+
+    public PlayerService(Repository<Player> repository)
+    {
+        _repository = repository;
+    }
+    
+    public Player? GetPlayerById(int id)
+    {
+        return _repository.GetById(id);
+    }
+
+    public IEnumerable<Player> GetAllPlayer()
+    {
+        return _repository.GetAll();
+    }
+    
+    public void AddPlayer(Player player)
+    {
+        _repository.Add(player);
+    }
+
+    public void UpdatePlayer(int id, Player updatedPlayer)
+    {
+        _repository.Update(id, updatedPlayer, (existing, updated) =>
+        {
+            existing.Name = updated.Name;
+            existing.UpdatedAt = updated.UpdatedAt;
+        });
+    }
+
+    public void DeletePlayer(int id)
+    {
+        _repository.Delete(id);
+    }
+}
