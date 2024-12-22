917ede5dbc77d5d7d4b415fc138a5a2a4a1ac59a
Sean Leon Maier <sean.maier@outlook.de>
2024-12-22 22:09:25 +0100

    Added Generalized TotalMatchDayService

---
+++ skat-back/services/TotalMatchDayService.cs
@@ -0,0 +1,18 @@
+﻿using skat_back.data;
+
+namespace skat_back.services;
+
+public class TotalMatchDayService
+{
+    private readonly Repository<TotalMatchDays> _repository;
+
+    public TotalMatchDayService(Repository<TotalMatchDays> repository)
+    {
+        _repository = repository;
+    }
+
+    public IEnumerable<TotalMatchDays> GetAllTotalMatchDays()
+    {
+        return _repository.GetAll();
+    }
+}
