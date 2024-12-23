465c369e50c7bd7cf026c25f4e0ededf4900723e
Sean Leon Maier <sean.maier@outlook.de>
2024-12-23 01:53:53 +0100

    Extracted logic to BaseController

---
+++ skat-back/controllers/MatchDayController.cs
@@ -0,0 +1,11 @@
+﻿using skat_back.data;
+using skat_back.services;
+
+namespace skat_back.controllers;
+
+public class MatchDayController : BaseController<MatchDay, MatchDayService>
+{
+    public MatchDayController(MatchDayService service) : base(service)
+    {
+    }
+}
