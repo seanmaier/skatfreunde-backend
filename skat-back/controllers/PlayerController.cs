7aee4d401b23b834c077239792f1b4de644013f3
Sean Leon Maier <sean.maier@outlook.de>
2024-12-23 01:58:55 +0100

    Added PlayerController

---
+++ skat-back/controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using skat_back.data;
+using skat_back.services;
+
+namespace skat_back.controllers;
+
+public class PlayerController: BaseController<Player, PlayerService>
+{
+    public PlayerController(PlayerService service) : base(service)
+    {
+        
+    }
+}
