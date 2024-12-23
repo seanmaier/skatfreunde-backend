aa8f40b0a665de3317b564d8b88887a9167b4344
Sean Leon Maier <sean.maier@outlook.de>
2024-12-23 01:05:27 +0100

    Added Service Interface

---
+++ skat-back/controllers/IService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace skat_back.controllers;
+
+public interface IService<in T> where T: class
+{
+    IActionResult GetAll();
+    IActionResult GetById(int id);
+    IActionResult Add(T entity);
+    IActionResult Update(int id, T entity);
+    IActionResult Delete(int id);
+}
