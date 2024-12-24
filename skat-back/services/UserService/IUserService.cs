9b9723658ffe49d2ab018c6f5deeab22aa76c674
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 02:14:47 +0100

    Added IUserService.cs

---
+++ skat-back/services/UserService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.controllers;
+using skat_back.data;
+
+namespace skat_back.services.UserService;
+
+public interface IUserService: IService<User>
+{
+    // Add specific methods here
+}
