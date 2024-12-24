fa17074db582c70d321b9e7ba438ba42e4e0a060
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 01:44:27 +0100

    Added MatchController.cs

---
+++ skat-back/controllers/MatchController.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.data;
+using skat_back.services.MatchService;
+
+namespace skat_back.controllers;
+
+public class MatchController: BaseController<Match, MatchService>
+{
+    public MatchController(MatchService service): base(service){}
+}
