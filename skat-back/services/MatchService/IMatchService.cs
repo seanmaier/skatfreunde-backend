d7ecbd1d0a7ff28305eb17998f056d2e0e0ba005
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 01:36:58 +0100

    Added IMatchService

---
+++ skat-back/services/MatchService/IMatchService.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.controllers;
+using skat_back.data;
+
+namespace skat_back.services.MatchService;
+
+public interface IMatchService: IService<Match>
+{
+    // Add specific methods here
+}
