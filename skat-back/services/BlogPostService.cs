2533e4618001517b083af96391e6b3f5b36aa24c
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 01:14:04 +0100

    Applied IService to BlogPostService

--- skat-back/services/BlogPostService.cs
+++ skat-back/services/BlogPostService.cs
@@ -1,33 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using skat_back.controllers;
 using skat_back.data;
 
 namespace skat_back.services;
 
-public class BlogPostService
+public class BlogPostService: IService<BlogPost>
 {
-    private readonly Repository<BlogPost> _repository;
+    private readonly IRepository<BlogPost> _repository;
 
-    public BlogPostService(Repository<BlogPost> repository)
+    public BlogPostService(IRepository<BlogPost> repository)
     {
         _repository = repository;
     }
 
-    public BlogPost? GetBlogPost(int id)
+    public BlogPost? GetById(int id)
     {
         return _repository.GetById(id);
     }
 
-    public IEnumerable<BlogPost> GetAllBlogPosts()
+    public IEnumerable<BlogPost> GetAll()
     {
         return _repository.GetAll();
     }
 
-    public void AddBlogPost(BlogPost blogPost)
+    public void Add(BlogPost blogPost)
     {
         _repository.Add(blogPost);
     }
 
-    public void UpdateBlogPost(BlogPost updatedBlogPost, int id)
+    public void Update(int id, BlogPost updatedBlogPost)
     {
         _repository.Update(id, updatedBlogPost, (existing, updated) =>
         {
@@ -42,7 +43,7 @@ public class BlogPostService
         });
     }
 
-    public void DeleteBlogPost(int id)
+    public void Delete(int id)
     {
         _repository.Delete(id);
     }
