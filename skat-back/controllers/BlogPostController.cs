8410bbe8bd256a8c08540340f0e5b1eba211eb4a
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 01:44:19 +0100

    Added BlogPostController.cs

---
+++ skat-back/controllers/BlogPostController.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.data;
+using skat_back.services.BlogPostService;
+
+namespace skat_back.controllers;
+
+public class BlogPostController: BaseController<BlogPost, BlogPostService>
+{
+    public BlogPostController(BlogPostService service): base(service){}
+}
