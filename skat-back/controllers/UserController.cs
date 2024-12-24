add2180d2cb405515780927d439ca18c3786ed4a
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 02:15:56 +0100

    Added UserController

---
+++ skat-back/controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.data;
+using skat_back.services.UserService;
+
+namespace skat_back.controllers;
+
+public class UserController: BaseController<User, UserService>
+{
+    public UserController(UserService service) : base(service){}
+}
