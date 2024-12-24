96a509d2d0fcbf58b9c47e869197e4b953419eff
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 02:35:43 +0100

    Created ServiceCollectionExtensions.cs

---
+++ skat-back/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using skat_back.services;
+using skat_back.services.BlogPostService;
+using skat_back.services.MatchDayService;
+using skat_back.services.MatchService;
+using skat_back.services.PlayerService;
+using skat_back.services.UserService;
+
+namespace skat_back;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<PlayerService>();
+        services.AddScoped<UserService>();
+        services.AddScoped<MatchService>();
+        services.AddScoped<BlogPostService>();
+        services.AddScoped<MatchDayService>();
+
+        return services;
+    }
+}
