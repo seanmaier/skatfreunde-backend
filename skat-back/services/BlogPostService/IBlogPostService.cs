b420a91ece16cee301d23fa97d7e72996bb8e07f
Sean Leon Maier <sean.maier@outlook.de>
2024-12-24 01:21:35 +0100

    Added Interface for BlogPosts

---
+++ skat-back/services/BlogPostService/IBlogPostService.cs
@@ -0,0 +1,9 @@
+﻿using skat_back.controllers;
+using skat_back.data;
+
+namespace skat_back.services.BlogPostService;
+
+public interface IBlogPostService: IService<BlogPost>
+{
+    // Add specific methods here
+}
