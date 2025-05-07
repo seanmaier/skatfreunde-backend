8074388f7fd0e99a0a4363f3c0c5a1753df6746b
seanmaier <sean.maier@outlook.de>
2025-05-07 07:35:30 +0200

    Add CreatePlayerDtoValidator for player name validation

---
+++ skat-back/utilities/validation/CreatePlayerDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using skat_back.DTO.PlayerDTO;
+using static skat_back.utilities.constants.ValidationConstants;
+
+namespace skat_back.utilities.validation;
+
+public class CreatePlayerDtoValidator : AbstractValidator<CreatePlayerDto>
+{
+    public CreatePlayerDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .Length(MinNameLength, MaxNameLength)
+            .WithMessage($"Name must be between {MinNameLength} and {MaxNameLength} characters long.");
+    }
+}
