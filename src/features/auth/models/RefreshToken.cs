b1ddeab96a4fc7e45738c5b53a4a95cfa021d38f
seanmaier <sean.maier@outlook.de>
2025-05-24 11:21:10 +0200

    Added data annotations for RefreshToken

--- src/features/auth/models/RefreshToken.cs
+++ src/features/auth/models/RefreshToken.cs
@@ -5,17 +5,22 @@ namespace skat_back.features.auth.models;
 public class RefreshToken
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public required string Token { get; set; }
+
+    [Required] [MaxLength(512)] public required string Token { get; set; }
+
     public required DateTime Expires { get; set; }
 
     public required DateTime Created { get; set; }
-    public required string CreatedByIp { get; set; } = string.Empty;
+
+    [MaxLength(64)] public required string CreatedByIp { get; set; } = string.Empty;
 
     public DateTime? Revoked { get; set; }
-    public string? RevokedByIp { get; set; }
-    public string? UserAgent { get; set; }
 
-    public string? ReplacedByToken { get; set; }
+    [MaxLength(64)] public string? RevokedByIp { get; set; }
+
+    [MaxLength(512)] public string? UserAgent { get; set; }
+
+    [MaxLength(512)] public string? ReplacedByToken { get; set; }
 
     // IsExpired is private because IsActive uses property to check if
     // the token is expired. IsActive should only be used outside
@@ -25,6 +30,7 @@ public class RefreshToken
 
     /*--------------------Navigation  Properties--------------------*/
 
-    [Required] public required string ApplicationUserId { get; set; }
+    [Required] [MaxLength(128)] public required string ApplicationUserId { get; set; }
+
     public ApplicationUser ApplicationUser { get; set; } = null!;
 }
